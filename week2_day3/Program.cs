1523ab704bc2250fed623f9ba950906316b90fd6
joderrick1022 <joderrick1022>
2017-10-04 22:05:20 -0400

    my favFood method()

--- week2_day3/Program.cs
+++ week2_day3/Program.cs
@@ -10,6 +10,24 @@ namespace week2_day3
     {
         static void Main(string[] args)
         {
+            ////when writing a method it has to be outside of static void main because it is a method
+            //int answer = Add(5,);
+            //Console.WriteLine(answer);
+            FavFood("for", "chicken");
+
+        }
+        public static int Add(int firstNumber , int secondNumber)
+
+        {
+            int sum = firstNumber + secondNumber;
+            return sum; // this is the keyword to using every method
+        }
+        public static void FavFood(string name, string food)
+        {
+            string conCad = name + " " + food;
+            Console.WriteLine(conCad);
+            
+            return   ;
         }
     }
 }
